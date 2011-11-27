71690f64d65e99c80249ffd9809d1085c1832cfd
Julio Jimenez <julio@kimonoten.net>
2011-11-27 04:43:13 -0300

    Agregada e implementada la lista circular.

---
+++ JTaxi/test/cl/pucv/eii/jtaxi/nounitarias/ListaCircularTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2011, Julio Jiménez, René Toro, José Vargas. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * 
+ * This file is part of JTaxi.
+ * 
+ * JTaxi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JTaxi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JTaxi.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package cl.pucv.eii.jtaxi.nounitarias;
+
+import java.util.ListIterator;
+
+import cl.pucv.eii.jtaxi.utilidades.listas.ListaCircular;
+
+public class ListaCircularTest {
+	public static void main(String...asdf){
+		ListaCircular<Integer> li = new ListaCircular<>();
+		
+		for(int i = 0; i<1;i++)
+			li.agregar(i);
+		
+		int i = 0;
+		for(ListIterator<Integer> itr = li.listIterator();itr.hasNext()&&i<0;){
+			System.out.print(itr.nextIndex()+"||");
+			System.out.println(itr.next());
+			i++;
+			
+		}
+		
+		i = 0;
+		for(ListIterator<Integer> itr = li.listIterator();itr.hasPrevious()&&i<100;){
+			System.out.print(itr.previousIndex()+"||");
+			System.out.println(itr.previous());
+			i++;
+			
+		}
+	}
+}
